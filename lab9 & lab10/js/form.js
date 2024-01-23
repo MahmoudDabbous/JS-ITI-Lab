8c8d646e54f6463bbed4d897ba67b2726df9d46e
MahmoudDabbous <mahmoud19728@gmail.com>
2024-01-23 16:33:19 +0200

    Lab 9&10

---
+++ lab9 & lab10/js/form.js
@@ -0,0 +1,23 @@
+function validateForm(e) {
+    var email = document.forms["form"]["email"].value;
+    var password = document.forms["form"]["password"].value;
+
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        document.getElementById("emailErr").innerHTML = "Please enter a valid email address.";
+        return false;
+    } else {
+        document.getElementById("emailErr").innerHTML = "";
+    }
+    
+    // Password validation
+    var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    if (!passwordRegex.test(password)) {
+        document.getElementById("passwordErr").innerHTML = "Password must contain at least 8 characters, including at least one lowercase letter, one uppercase letter, and one number.";
+        return false;
+    } else {
+        document.getElementById("passwordErr").innerHTML = "";
+    }
+
+    return true;
+}
